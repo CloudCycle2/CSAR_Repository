a2c9ae8f3ff596466f7265b170ecd2d6b7569538
Fabian Toth <tothfn@studi.informatik.uni-stuttgart.de>
2014-11-25 15:05:34 +0100

    New eclipse project

---
+++ csarrepo/src/csarrepo/KeyboardBean.java
@@ -0,0 +1,23 @@
+package csarrepo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class KeyboardBean {
+ 
+   private String value;
+ 
+   public String getValue() {
+       System.out.println("KeyboardBean::reading value: " + value);
+        
+       return value;
+   }
+ 
+   public void setValue(String value) {
+       System.out.println("KeyboardBean::setting value: " + value);
+        
+       this.value = value;
+   }   
+}
