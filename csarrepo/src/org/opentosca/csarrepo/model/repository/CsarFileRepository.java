a8a4092efbb1518ba94301175c4b6cd7ef715130
Dennis Przytarski <dennis@przytarski.com>
2014-12-03 22:13:35 +0100

    implemented repository for csarfile, fixes #18

---
+++ csarrepo/src/org/opentosca/csarrepo/model/repository/CsarFileRepository.java
@@ -0,0 +1,117 @@
+package org.opentosca.csarrepo.model.repository;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.opentosca.csarrepo.model.CsarFile;
+
+/**
+ * Class to avoid direct access of the hibernate active records for CSAR file.
+ * 
+ * @author Markus Eisele, Dennis Przytarski
+ *
+ */
+public class CsarFileRepository {
+
+	/**
+	 * Returns a csar file for the given id.
+	 * 
+	 * @param id
+	 * @return CsarFile
+	 */
+	public CsarFile getbyId(long id) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		CsarFile csarFile = null;
+		try {
+			tx = session.beginTransaction();
+			csarFile = (CsarFile) session.get(CsarFile.class, id);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return csarFile;
+	}
+
+	/**
+	 * Gets all CSAR files.
+	 * 
+	 * @return List of CSAR files.
+	 */
+	public List<CsarFile> getAll() {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		List<CsarFile> csarFileList = null;
+		try {
+			tx = session.beginTransaction();
+			csarFileList = (List<CsarFile>) session.createQuery("from CsarFile").list();
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return csarFileList;
+	}
+
+	/**
+	 * @param CsarFile
+	 *            to be stored
+	 * @return id of the saved csar file
+	 * @throws PersistenceException
+	 *             upon problems commiting the underlying transaction
+	 */
+	public long save(CsarFile csarFile) throws PersistenceException {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.saveOrUpdate(csarFile);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			throw new PersistenceException(e);
+		} finally {
+			session.close();
+		}
+		return csarFile.getId();
+	}
+
+	/**
+	 * @param CsarFile
+	 * @throws PersistenceException
+	 *             upon problems committing the underlying transaction
+	 */
+	public void delete(CsarFile csarFile) throws PersistenceException {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.delete(csarFile);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			throw new PersistenceException(e);
+		} finally {
+			session.close();
+		}
+	}
+
+}
