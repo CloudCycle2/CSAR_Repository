d1817d5df29734a30a83f34259acd74eeaeabb57
Thomas Kosch <thomaskosch90@gmail.com>
2014-12-03 12:03:31 +0100

    Created CsarFile hibernate raw model refs #16

--- csarrepo/src/org/opentosca/csarrepo/model/User.java
+++ csarrepo/src/org/opentosca/csarrepo/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 /**
  * This is a first example for a model using hibernate
  * 
@@ -12,12 +13,12 @@ import javax.persistence.Id;
  */
 @Entity
 public class User {
-	
+
 	private long id;
 	private String userName;
 	private String userPassword;
 	private String userMail;
-	
+
 	/**
 	 * @return the id
 	 */
@@ -26,51 +27,57 @@ public class User {
 	public long getId() {
 		return id;
 	}
+
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the userName
 	 */
 	public String getUserName() {
 		return userName;
 	}
+
 	/**
-	 * @param userName the userName to set
+	 * @param userName
+	 *            the userName to set
 	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	/**
 	 * @return the userPassword
 	 */
 	public String getUserPassword() {
 		return userPassword;
 	}
+
 	/**
-	 * @param userPassword the userPassword to set
+	 * @param userPassword
+	 *            the userPassword to set
 	 */
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
 	/**
 	 * @return the userMail
 	 */
 	public String getUserMail() {
 		return userMail;
 	}
+
 	/**
-	 * @param userMail the userMail to set
+	 * @param userMail
+	 *            the userMail to set
 	 */
 	public void setUserMail(String userMail) {
 		this.userMail = userMail;
 	}
-	
-	
-
-	
-	
 }
