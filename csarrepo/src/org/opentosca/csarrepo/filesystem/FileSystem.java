64cb906c8e1d8722660d696b973b3f53237d9a5f
Fabian Toth <tothfn@studi.informatik.uni-stuttgart.de>
2014-12-05 22:37:46 +0100

    refs #23 fixed the wrong formatting

--- csarrepo/src/org/opentosca/csarrepo/filesystem/FileSystem.java
+++ csarrepo/src/org/opentosca/csarrepo/filesystem/FileSystem.java
@@ -22,10 +22,7 @@ public class FileSystem {
 
 	private static final Logger LOGGER = LogManager.getLogger(FileSystem.class);
 
-	private static final String BASE_PATH = System
-			.getProperty("java.io.tmpdir")
-			+ File.separator
-			+ "csarrepo"
+	private static final String BASE_PATH = System.getProperty("java.io.tmpdir") + File.separator + "csarrepo"
 			+ File.separator;
 
 	private static final String FILE_EXTENSION = ".csar";
@@ -44,8 +41,7 @@ public class FileSystem {
 		String absPath = BASE_PATH + generatedFileName + FILE_EXTENSION;
 
 		try {
-			Files.move(file.toPath(), Paths.get(absPath),
-					StandardCopyOption.ATOMIC_MOVE);
+			Files.move(file.toPath(), Paths.get(absPath), StandardCopyOption.ATOMIC_MOVE);
 		} catch (IOException e) {
 			LOGGER.error(e.getMessage(), e);
 			throw e;
