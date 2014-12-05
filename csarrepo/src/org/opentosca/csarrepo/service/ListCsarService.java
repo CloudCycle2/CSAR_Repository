0e5dde7aabe437053b31928149c46961e1e6b22e
Dennis Przytarski <dennis@przytarski.com>
2014-12-05 14:12:29 +0100

    moved operation to constructor, refs #22

--- csarrepo/src/org/opentosca/csarrepo/service/ListCsarService.java
+++ csarrepo/src/org/opentosca/csarrepo/service/ListCsarService.java
@@ -11,23 +11,25 @@ import org.opentosca.csarrepo.model.repository.CsarRepository;
 public class ListCsarService extends AbstractService {
 
 	private final CsarRepository csarRepository = new CsarRepository();
+	private List<Csar> csarFiles = null;
 
 	/**
 	 * @param userId
 	 */
 	public ListCsarService(long userId) {
 		super(userId);
+		try {
+			this.csarFiles = csarRepository.getAll();
+		} catch (Exception e) {
+			this.addError(e.getMessage());
+		}
 	}
 
 	/**
 	 * @return List of CSARs
 	 */
 	public List<Csar> getResult() {
-		try {
-			return csarRepository.getAll();
-		} catch (Exception e) {
-			this.addError(e.getMessage());
-		}
-		return null;
+		return csarFiles;
 	}
+
 }
