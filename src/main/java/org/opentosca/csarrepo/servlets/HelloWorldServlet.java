0a7f92a34d80550e5283384f061dd33912fec645
Dennis Przytarski <dennis@przytarski.com>
2014-12-12 16:59:06 +0100

    added freemarker and implemented show csar demo, refs #72

--- src/main/java/org/opentosca/csarrepo/servlets/HelloWorldServlet.java
+++ src/main/java/org/opentosca/csarrepo/servlets/HelloWorldServlet.java
@@ -1,11 +1,22 @@
 package org.opentosca.csarrepo.servlets;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.opentosca.csarrepo.service.ListCsarService;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 /**
  * Servlet implementation class HelloWorldServlet
@@ -13,27 +24,36 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+	Configuration cfg = new Configuration(Configuration.VERSION_2_3_21);
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public HelloWorldServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.doPost(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("<html><body>Hello World Content</body></html>");
+		cfg.setDirectoryForTemplateLoading(new File(this.getServletContext().getRealPath("/")));
+		Template template = cfg.getTemplate("helloworldservlet.ftl");
+		try {
+			ListCsarService csarService = new ListCsarService(0L);
+			if (csarService.hasErrors()) {
+				// FIXME, get all errors - not only first
+				throw new ServletException("csarService has errors:" + csarService.getErrors().get(0));
+			}
+			Map<String, Object> root = new HashMap<String, Object>();
+			root.put("csars", csarService.getResult());
+			template.process(root, response.getWriter());
+		} catch (TemplateException e) {
+			response.getWriter().print(e.getMessage());
+		}
 	}
 
 }
