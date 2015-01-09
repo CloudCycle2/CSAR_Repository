8b74118499fff8a6396b9816432ed7726e084112
Dennis Przytarski <dennis@przytarski.com>
2015-01-09 14:26:17 +0100

    extended deleteCsarFileService for deleting files from database and file system, refs #133

--- src/main/java/org/opentosca/csarrepo/service/DeleteCsarFileService.java
+++ src/main/java/org/opentosca/csarrepo/service/DeleteCsarFileService.java
@@ -1,9 +1,14 @@
 package org.opentosca.csarrepo.service;
 
+import java.util.UUID;
+
 import org.opentosca.csarrepo.exception.PersistenceException;
+import org.opentosca.csarrepo.filesystem.FileSystem;
 import org.opentosca.csarrepo.model.Csar;
 import org.opentosca.csarrepo.model.CsarFile;
+import org.opentosca.csarrepo.model.HashedFile;
 import org.opentosca.csarrepo.model.repository.CsarFileRepository;
+import org.opentosca.csarrepo.model.repository.FileSystemRepository;
 
 /**
  * Deletes the given CSAR file.
@@ -25,10 +30,23 @@ public class DeleteCsarFileService extends AbstractService {
 		this.csarFileId = csarFileId;
 
 		try {
+			FileSystemRepository fileSystemRepository = new FileSystemRepository();
 			CsarFileRepository csarFileRepository = new CsarFileRepository();
 			CsarFile csarFile = csarFileRepository.getbyId(this.csarFileId);
 			this.csar = csarFile.getCsar();
+
+			HashedFile hashedFile = csarFile.getHashedFile();
+
+			// csar file must be deleted before hashed file
 			csarFileRepository.delete(csarFile);
+
+			// delete hashed file from database and file system, if necessary
+			if (fileSystemRepository.isHashDeletable(hashedFile.getHash())) {
+				fileSystemRepository.delete(hashedFile);
+				FileSystem fileSystem = new FileSystem();
+				fileSystem.deleteFromFileSystem(UUID.fromString(hashedFile.getFileName()));
+			}
+
 			this.returnValue = true;
 		} catch (PersistenceException e) {
 			this.addError(e.getMessage());
