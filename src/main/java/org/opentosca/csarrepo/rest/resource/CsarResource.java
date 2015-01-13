2b30cb8592f156d2084b340af2cedc6f56ffe397
eiselems <marcus.eisele@gmail.com>
2015-01-13 13:21:12 +0100

    add upload file for REST

--- src/main/java/org/opentosca/csarrepo/rest/resource/CsarResource.java
+++ src/main/java/org/opentosca/csarrepo/rest/resource/CsarResource.java
@@ -1,10 +1,13 @@
 package org.opentosca.csarrepo.rest.resource;
 
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +19,8 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.opentosca.csarrepo.model.Csar;
 import org.opentosca.csarrepo.model.CsarFile;
 import org.opentosca.csarrepo.rest.model.CsarEntry;
@@ -23,6 +28,7 @@ import org.opentosca.csarrepo.rest.model.SimpleXLink;
 import org.opentosca.csarrepo.rest.util.LinkBuilder;
 import org.opentosca.csarrepo.service.DeleteCsarService;
 import org.opentosca.csarrepo.service.ShowCsarService;
+import org.opentosca.csarrepo.service.UploadCsarFileService;
 
 public class CsarResource {
 
@@ -65,15 +71,15 @@ public class CsarResource {
 		LOGGER.debug("Accessing csar<id:{},name:{}>", csar.getId(), csar.getName());
 		return Response.ok(csarEntry).build();
 	}
-	
+
 	@DELETE
 	public Response deleteCsar() {
 		DeleteCsarService service = new DeleteCsarService(0, this.id);
-		
-		if(service.hasErrors()) {
+
+		if (service.hasErrors()) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(service.getErrors().get(0)).build();
 		}
-		
+
 		return Response.ok().build();
 	}
 
@@ -82,5 +88,36 @@ public class CsarResource {
 	public Object getCsarFile(@PathParam("id") long csarfileID, @Context UriInfo uriInfo) {
 		// TODO: add warning if longID = -1;
 		return new CsarFileResource(uriInfo, this.id, csarfileID);
+	}
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		if (null == uploadedInputStream) {
+			// TODO: logger
+			return Response.serverError().entity("The stream is null.").build();
+		}
+
+		if (null == fileDetail) {
+			return Response.serverError().entity("The file details are null.").build();
+		}
+
+		String csarName = fileDetail.getFileName();
+		UploadCsarFileService upService = new UploadCsarFileService(0L, this.id, uploadedInputStream, csarName);
+		// TODO, think about better Exceptionhandling (currently we
+		// just take first Exception)
+		if (upService.hasErrors()) {
+			return Response.serverError().entity("UploadCsarService has Errors: " + upService.getErrors().get(0))
+					.build();
+		}
+
+		CsarFile csarFile = upService.getResult();
+
+		LOGGER.info("Post for uploading a new CSAR as file with name \"" + fileDetail.getFileName() + "\" with size ");
+
+		return Response.ok().entity(LinkBuilder.linkToCsarFile(uriInfo, this.id, csarFile.getId())).build();
+
 	}
 }
