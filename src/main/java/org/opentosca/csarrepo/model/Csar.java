481bb9793d17162a1c4ca5bad51fc4f577e37d98
Dennis Przytarski <dennis@przytarski.com>
2015-01-16 14:07:12 +0100

    implemented winery server model, refs #166

--- src/main/java/org/opentosca/csarrepo/model/Csar.java
+++ src/main/java/org/opentosca/csarrepo/model/Csar.java
@@ -3,12 +3,16 @@ package org.opentosca.csarrepo.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -34,6 +38,10 @@ public class Csar {
 
 	@OneToMany(fetch = FetchType.EAGER, mappedBy = "csar")
 	private List<CsarFile> csarFiles;
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "csar_winery_server", joinColumns = { @JoinColumn(name = "csar_id") }, inverseJoinColumns = { @JoinColumn(name = "winery_server_id") })
+	private List<WineryServer> wineryServers;
 
 	public Csar() {
 		this.csarFiles = new ArrayList<CsarFile>();
---
+++ src/main/java/org/opentosca/csarrepo/model/WineryServer.java
@@ -0,0 +1,96 @@
+package org.opentosca.csarrepo.model;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * 
+ * Hibernate annotated class for Winery Server
+ * 
+ * @author Dennis Przytarski, Thomas Kosch (mail@thomaskosch.com)
+ *
+ */
+
+@Entity
+@Table(name = "winery_server")
+public class WineryServer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "winery_server_id")
+	private long id;
+
+	@Column(name = "address")
+	private InetSocketAddress address;
+
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "wineryServers")
+	private List<Csar> csars;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public InetSocketAddress getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            to set
+	 */
+	public void setAddress(InetSocketAddress address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the csars
+	 */
+	public List<Csar> getCsars() {
+		return csars;
+	}
+
+	/**
+	 * @param csars
+	 *            to set
+	 */
+	public void setCsars(List<Csar> csars) {
+		this.csars = csars;
+	}
+
+	/**
+	 * @return the users
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user
+	 *            to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
