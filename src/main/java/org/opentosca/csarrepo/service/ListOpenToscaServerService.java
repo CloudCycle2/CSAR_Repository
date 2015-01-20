818a0ab4e4a3d441739e589f06d3423512f7824a
eiselems <marcus.eisele@gmail.com>
2015-01-20 14:03:24 +0100

    create ListOpenToscaServerService refs #186

---
+++ src/main/java/org/opentosca/csarrepo/service/ListOpenToscaServerService.java
@@ -0,0 +1,41 @@
+package org.opentosca.csarrepo.service;
+
+import java.util.List;
+
+import org.opentosca.csarrepo.exception.PersistenceException;
+import org.opentosca.csarrepo.model.OpenToscaServer;
+import org.opentosca.csarrepo.model.repository.OpenToscaServerRepository;
+
+/**
+ * Service which returns a List of OpenToscaServers for a specific user
+ * 
+ * @author Marcus Eisele
+ */
+public class ListOpenToscaServerService extends AbstractService {
+
+	private final OpenToscaServerRepository openToscaServerRepository = new OpenToscaServerRepository();
+	private List<OpenToscaServer> openToscaServers = null;
+
+	/**
+	 * @param userId
+	 */
+	public ListOpenToscaServerService(long userId) {
+		super(userId);
+		try {
+			// TODO: use userId instead of .getAll()
+			this.openToscaServers = openToscaServerRepository.getAll();
+		} catch (PersistenceException e) {
+			this.addError(e.getMessage());
+		}
+	}
+
+	/**
+	 * @return List of OpenToscaServers
+	 */
+	public List<OpenToscaServer> getResult() {
+		super.logInvalidResultAccess("getResult");
+
+		return this.openToscaServers;
+	}
+
+}
