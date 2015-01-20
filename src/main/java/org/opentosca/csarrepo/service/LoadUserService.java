dbc1bf3c8cb67cdc00866ac8102b5e3d228aea8f
Dennis Przytarski <dennis@przytarski.com>
2015-01-20 11:55:37 +0100

    implemented load user service, refs #178

---
+++ src/main/java/org/opentosca/csarrepo/service/LoadUserService.java
@@ -0,0 +1,46 @@
+package org.opentosca.csarrepo.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opentosca.csarrepo.exception.PersistenceException;
+import org.opentosca.csarrepo.model.User;
+import org.opentosca.csarrepo.model.repository.UserRepository;
+
+/**
+ * 
+ * @author Dennis Przytarski
+ */
+public class LoadUserService extends AbstractService {
+
+	private static final Logger LOGGER = LogManager.getLogger(LoadUserService.class);
+	User user;
+
+	/**
+	 * Loads an user by given id.
+	 * 
+	 * @param id
+	 */
+	public LoadUserService(long id) {
+		super(0);
+
+		UserRepository userRepository = new UserRepository();
+
+		try {
+			this.user = userRepository.getById(id);
+		} catch (PersistenceException e) {
+			this.addError(e.getMessage());
+			LOGGER.error(e);
+		}
+	}
+
+	/**
+	 * 
+	 * @return the user
+	 */
+	public User getResult() {
+		super.logInvalidResultAccess("getResult");
+
+		return this.user;
+	}
+
+}
