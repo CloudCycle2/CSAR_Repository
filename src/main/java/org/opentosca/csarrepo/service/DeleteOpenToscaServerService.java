12447fc7026629bfc759a76ad3880eb2f5e659a7
eiselems <marcus.eisele@gmail.com>
2015-01-23 10:09:44 +0100

    add servlet, service to delete OTServers refs #220

---
+++ src/main/java/org/opentosca/csarrepo/service/DeleteOpenToscaServerService.java
@@ -0,0 +1,46 @@
+package org.opentosca.csarrepo.service;
+
+import org.opentosca.csarrepo.exception.PersistenceException;
+import org.opentosca.csarrepo.model.OpenToscaServer;
+import org.opentosca.csarrepo.model.repository.OpenToscaServerRepository;
+
+/**
+ * Service deleting OpenToscaServers
+ * 
+ * @author eiselems (marcus.eisele@gmail.com)
+ *
+ */
+public class DeleteOpenToscaServerService extends AbstractService {
+
+	/**
+	 * @param userId
+	 * @param opentoscaServerId
+	 */
+	public DeleteOpenToscaServerService(long userId, long opentoscaServerId) {
+		super(userId);
+
+		try {
+			OpenToscaServerRepository otServerRepo = new OpenToscaServerRepository();
+			OpenToscaServer otServer = otServerRepo.getbyId(opentoscaServerId);
+
+			if (otServer == null) {
+				this.addError("invalidOpenToscaServer");
+				return;
+			}
+
+			otServerRepo.delete(otServer);
+		} catch (PersistenceException e) {
+			this.addError(e.getMessage());
+		}
+	}
+
+	/**
+	 * @return status of deletion
+	 */
+	public boolean getResult() {
+		super.logInvalidResultAccess("getResult");
+
+		return !super.hasErrors();
+	}
+
+}
