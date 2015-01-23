34489fe6f971c6c1fde55d12b84b4cc0b2e26170
Dennis Przytarski <dennis@przytarski.com>
2015-01-23 13:48:22 +0100

    fixed bug: add empty constructor to csar

--- src/main/java/org/opentosca/csarrepo/model/Csar.java
+++ src/main/java/org/opentosca/csarrepo/model/Csar.java
@@ -56,6 +56,9 @@ public class Csar {
 	@OneToMany(fetch = FetchType.EAGER, mappedBy = "csarId")
 	private List<CsarUser> csarUser;
 
+	public Csar() {
+	}
+
 	public Csar(Long id) {
 		this.id = id;
 	}
