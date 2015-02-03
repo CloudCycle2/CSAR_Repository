aaae2d65e1f9f1162c50f1d983c371e11a0e6f6e
Dennis Przytarski <dennis@przytarski.com>
2015-02-03 13:53:54 +0100

    formatter

--- src/main/java/org/opentosca/csarrepo/servlet/UpdateOpenToscaServerServlet.java
+++ src/main/java/org/opentosca/csarrepo/servlet/UpdateOpenToscaServerServlet.java
@@ -16,8 +16,7 @@ import org.opentosca.csarrepo.service.UpdateOpenToscaServerService;
 @WebServlet(UpdateOpenToscaServerServlet.PATH)
 public class UpdateOpenToscaServerServlet extends AbstractServlet {
 
-	private static final Logger LOGGER = LogManager
-			.getLogger(UpdateOpenToscaServerServlet.class);
+	private static final Logger LOGGER = LogManager.getLogger(UpdateOpenToscaServerServlet.class);
 	private static final String PARAM_OPEN_TOSCA_SERVER_NAME = "openToscaServerName";
 	private static final String PARAM_OPEN_TOSCA_SERVER_URL = "address";
 	private static final String PARAM_OPEN_TOSCA_SERVER_ID = "openToscaServerId";
@@ -28,15 +27,14 @@ public class UpdateOpenToscaServerServlet extends AbstractServlet {
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.sendError(405, "Method Not Allowed");
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
 
 		try {
 			checkUserAuthentication(request, response);
@@ -44,30 +42,22 @@ public class UpdateOpenToscaServerServlet extends AbstractServlet {
 			return;
 		}
 
-		Long openToscaServerId = Long.parseLong(request
-				.getParameter(PARAM_OPEN_TOSCA_SERVER_ID));
-		String openToscaServerName = request
-				.getParameter(PARAM_OPEN_TOSCA_SERVER_NAME);
-		String OpenToscaServerAddress = request
-				.getParameter(PARAM_OPEN_TOSCA_SERVER_URL);
+		Long openToscaServerId = Long.parseLong(request.getParameter(PARAM_OPEN_TOSCA_SERVER_ID));
+		String openToscaServerName = request.getParameter(PARAM_OPEN_TOSCA_SERVER_NAME);
+		String OpenToscaServerAddress = request.getParameter(PARAM_OPEN_TOSCA_SERVER_URL);
 
-		UpdateOpenToscaServerService service = new UpdateOpenToscaServerService(
-				0L, openToscaServerId, openToscaServerName,
-				OpenToscaServerAddress);
+		UpdateOpenToscaServerService service = new UpdateOpenToscaServerService(0L, openToscaServerId,
+				openToscaServerName, OpenToscaServerAddress);
 
-		LOGGER.debug("Request to update open tosca server " + openToscaServerId
-				+ " handeled by servlet");
+		LOGGER.debug("Request to update open tosca server " + openToscaServerId + " handeled by servlet");
 
 		if (service.hasErrors()) {
 			response.getWriter().write(service.getErrors().get(0));
 			return;
 		}
 
-		this.redirect(
-				request,
-				response,
-				OpenToscaServerDetailsServlet.PATH.replaceFirst("\\*",
-						Long.toString(openToscaServerId)));
+		this.redirect(request, response,
+				OpenToscaServerDetailsServlet.PATH.replaceFirst("\\*", Long.toString(openToscaServerId)));
 
 	}
 }
